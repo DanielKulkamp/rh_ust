5fe84ee2ac3f44215aa3e6e4089a30b1eca14a68
Daniel Clemes Kulkamp <daniel.kulkamp@gmail.com>
2020-07-20 23:45:08 -0300

    not working yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to rh_ust!");
+    println!("List of commands:");
+    println!("add -> \"<full employee name>\" to \"<department name>\"");
+    println!("list all");
+    println!("list employees in <department name>");
+    println!("quit");
+
+    let mut departments : HashMap<&str, Vec<String>> = HashMap::new();
+
+    let mut input = String::new();
+    loop {
+    	input.clear();
+    	std::io::stdin().read_line(&mut input).expect("failed to read line")	;
+    	let mut input_words = input.split_whitespace();
+    	let command = match input_words.next() {
+    		Some(word) => {
+    			match word {
+    				"add" | "list" | "quit" => word,
+    				_ => {
+    					println!("Invalid command {}", word );
+    					continue;
+    				}
+    			}
+    		},
+    		None => {
+    			continue
+    		}
+    	};
+    	match command {
+    		"add" => parse_add(words),
+    		_ => ()
+    	}
+    }
+}
+
+fn parse_add(parameters) -> (String, String) {
+	let mut employee = String::new();
+	if.parametes.next().starts_with("\"") {
+		
+	}
+}
+
+fn input_valid(valid : Vec<String>) -> String {
+	let mut input = String::new();
+	loop {
+		input.clear();
+		std::io::stdin().read_line(&mut input).expect("failed to read line");
+		let mut words = input.split_whitespace();
+		let command = match words.next() {
+			Some(word) => {
+				break word.to_string();
+			},
+			None => continue,
+		};
+	}
 }

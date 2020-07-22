bdf8c9794a430137d550a969476274a6ce292df9
Daniel Clemes Kulkamp <daniel.kulkamp@gmail.com>
2020-07-22 19:58:35 -0300

    using enums for commands

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,73 @@ use std::collections::HashMap;
 
 fn main() {
     println!("Welcome to rh_ust!");
-    println!("List of commands:");
-    println!("add");
-    println!("list");
-    println!("quit");
+    help();
 
     let mut departments : HashMap<&str, Vec<String>> = HashMap::new();
 
-    let mut input = String::new();
     loop {
-    	input.clear();
-    	std::io::stdin().read_line(&mut input).expect("failed to read line")	;
-    	let mut input_words = input.split_whitespace();
-    	let command = match input_words.next() {
-    		Some(word) => {
-    			match word {
-    				"add" | "list" | "quit" => word,
-    				_ => {
-    					println!("Invalid command {}", word );
-    					continue;
-    				}
-    			}
-    		},
-    		None => {
-    			continue
-    		}
-    	};
-    	match command {
-    		"add" => {
-	    		add()
-	    	},
-    		_ => ()
+    	match get_command(){
+    		Some(Command::Add) => add(),
+    		Some(Command::List) => list(),
+    		Some(Command::Quit) => break,
+    		Some(Command::Help) => help(),
+    		None => continue,
     	}
+
     }
 }
 
-fn add(parameters) -> (String, String) {
-	let mut employee = String::new();
-	if.parametes.next().starts_with("\"") {
 
-	}
+enum Command  {
+	Add,
+	List,
+	Quit,
+	Help,
 }
 
-fn input_valid(valid : Vec<String>) -> String {
-	let mut input = String::new();
+fn add() {
+	println!("dummy function add");
+}
+
+fn list() {
+	println!("dummy function list");
+}
+
+fn help() {
+	println!("List of commands:");
+    println!("add");
+    println!("list");
+    println!("quit");
+}
+
+fn get_command() -> Option<Command> {
 	loop {
-		input.clear();
-		std::io::stdin().read_line(&mut input).expect("failed to read line");
+		let input = user_input().unwrap_or_else(|error| {
+			println!("Failed to read line! try again... or press Ctrl-C");
+			"".to_string()
+		});	
 		let mut words = input.split_whitespace();
 		let command = match words.next() {
 			Some(word) => {
-				break word.to_string();
+				match word {
+					"add" => return Some(Command::Add),
+					"list" => return Some(Command::List),
+					"help" => return Some(Command::Help),
+					"quit" => return Some(Command::Quit),
+					_ => {
+						println!("Invalid command.");
+						continue;
+					}
+				}
 			},
 			None => continue,
 		};
 	}
+}
+
+
+fn user_input() -> Result<String, std::io::Error> {
+	let mut text = String::new();
+	std::io::stdin().read_line(&mut text)?;
+	Ok(text)
 }
